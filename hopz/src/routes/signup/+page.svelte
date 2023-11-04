f1ccf407bc62b5e9f073eed4e5413f5435ad1035
mikevanwinkleatsonos <mike.vanwinkle@sonos.com>
2023-11-04 12:02:51 -0700

    initial front end

---
+++ hopz/src/routes/signup/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Get Started</title>
+	<meta name="description" content="Getting started giving beer love with hopheads" />
+</svelte:head>
+
+<div id="text-column">
+  <h1>Create your HopHeads account</h1>
+  <p>
+     HopHeads is a community of beer lovers. We are a place for people to share their beer experiences and connect with others who love beer.
+     HopHeads is not a place here to replace sites like Untapped  but rather to create engaging experiences for beer lovers.
+  </p>
+</div>
